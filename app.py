56521456ef8f01eaf5a4f2566e169ff8806d9cf6
Naing Ye Minn <naingyeminn@gmail.com>
2022-03-30 08:44:58 +0000

    update message

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-APP_MSG = os.environ.get('APP_MSG', 'World')
+APP_MSG = os.environ.get('HOSTNAME', 'hello-flask')
 
 @app.route("/")
 def hello():
-  return f"Hello {APP_MSG}!\n"
+  return f"Welcome to {APP_MSG}!\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
