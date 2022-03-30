1ee15d3de1275bdb327275049f0113f7cc1c9071
Naing Ye Minn <naingyeminn@gmail.com>
2022-03-30 10:38:30 +0000

    update variable name

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-APP_MSG = os.environ.get('HOSTNAME', 'hello-flask')
+APP_HOSTNAME = os.environ.get('HOSTNAME', 'hello-flask')
 
 @app.route("/")
 def hello():
-  return f"<h1>Welcome to {APP_MSG}</h1>\n <h2>This is a new day!</h2>\n"
+  return f"<h1>Welcome to {APP_HOSTNAME}</h1>\n <h2>This is a new day!</h2>\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
