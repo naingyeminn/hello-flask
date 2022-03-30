00695e55f8a83a11b3ec726650986228161d26e5
Naing Ye Minn <naingyeminn@gmail.com>
2022-03-30 16:21:13 +0630

    add new line

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ APP_MSG = os.environ.get('HOSTNAME', 'hello-flask')
 
 @app.route("/")
 def hello():
-  return f"Welcome to {APP_MSG}!\n"
+  return f"Welcome to {APP_MSG}!\n This is a new day!\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
